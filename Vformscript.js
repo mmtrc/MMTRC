fa2da6bf582b480f44c74336ae722c7b4f204e94
ColeConsulting1786 <154651500+ColeConsulting1786@users.noreply.github.com>
2025-05-09 15:24:46 -0400

    Update Vformscript.js

--- Vformscript.js
+++ Vformscript.js
@@ -24,7 +24,7 @@ document.getElementById('volunteerForm').addEventListener('submit', async functi
     };
 
     try {
-        const response = await fetch('https://script.google.com/a/macros/trcmm.org/s/AKfycbx-3q6lhQ88mJqVjB65lcxYpgHAnY3rymuWjNijInEZxGDRctFlF0_4FpbQp8Vh5WnH/exec', {
+        const response = await fetch('https://script.google.com/a/macros/trcmm.org/s/AKfycbwCmhcvVPS_Q8bI55aZ3CZAxG68npmRXfNIHMBt78LUgeJRCE3XQQgFIUBJ0DNr1g8/exec', {
             method: 'POST',
             mode: 'no-cors', // Note: Apps Script doesn't return CORS headers, so this prevents errors but also hides success/failure.
             headers: {
